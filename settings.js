22f01b12242b4b3292d333fa2ec959514c3f9e6c
po958 <pottersusan526@gmail.com>
2025-07-19 06:22:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=kmdwUbCR#aRYsQSQRBsIp7mPRV1KRZSWuuENacTKq01GQuRv5cKc' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
-MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
+MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "true" : process.env.AUTO_REPLY,
 AI_REPLAY: process.env.AI_REPLAY === undefined ? "true" : process.env.AI_REPLAY,//AI_REPLAY KARANA KOTASA ADD KARA
